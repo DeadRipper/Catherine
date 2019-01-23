169dbdef752db349bbffeb009497cc17cce9c868
DeadRipper <hardyextrim@gmail.com>
2019-01-23 22:18:22 +0200

    Add files via upload

---
+++ Catherine/Catherine/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Catherine
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(206, 71);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(61, 17);
+			this.checkBox1.TabIndex = 15;
+			this.checkBox1.Text = "Micro ?";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 71);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(187, 20);
+			this.textBox1.TabIndex = 14;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(255, 12);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 13;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(118, 38);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(64, 13);
+			this.label2.TabIndex = 12;
+			this.label2.Text = "00:00:00:00";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(20, 38);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 13);
+			this.label1.TabIndex = 11;
+			this.label1.Text = "Record Time";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(110, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(91, 23);
+			this.button2.TabIndex = 10;
+			this.button2.Text = "Stop Record";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(92, 23);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Begin Record";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(366, 133);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Catherine/Catherine/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NAudio;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using NAudio.FileFormats;
+using System.Speech.Recognition;
+using System.Speech.AudioFormat;
+using System.Globalization;
+using System.Speech.Synthesis;
+using System.Threading;
+
+namespace Catherine
+{
+	public partial class Form1 : Form
+	{
+		WaveIn waveIn;
+		WaveFileWriter waveFileWriter;
+
+		string output_file = "Sound_FIle.wav";
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		void StopRecord()
+		{
+			MessageBox.Show("Stop");
+			waveIn.StopRecording();
+		}
+
+		void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+		{
+			if (this.InvokeRequired)
+				this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailable), sender, e);
+			else
+				waveFileWriter.Write(e.Buffer, 0, e.BytesRecorded);
+		}
+
+		private void waveIn_RecordingStopped(object sender, EventArgs e)
+		{
+			if (this.InvokeRequired)
+				this.BeginInvoke(new EventHandler(waveIn_RecordingStopped), sender, e);
+			else
+			{
+				waveIn.Dispose();
+				waveIn = null;
+				waveFileWriter.Close();
+				waveFileWriter = null;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				waveIn = new WaveIn();
+				waveIn.DeviceNumber = 0;
+				waveIn.DataAvailable += waveIn_DataAvailable;
+				waveIn.RecordingStopped += waveIn_RecordingStopped;
+				waveIn.WaveFormat = new WaveFormat(8000, 1);
+				waveFileWriter = new WaveFileWriter(output_file, waveIn.WaveFormat);
+				waveIn.StartRecording();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (waveIn != null)
+				StopRecord();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
+
+			Grammar dictationGrammar = new DictationGrammar();
+			recognizer.LoadGrammar(dictationGrammar);
+			if (checkBox1.Checked == true)
+				recognizer.SetInputToDefaultAudioDevice();
+			else
+				recognizer.SetInputToWaveFile(@"C:\Users\hardy\source\repos\VoiceSaver_\VoiceSaver_\bin\Debug\Sound_File.wav");
+
+			RecognitionResult result = recognizer.Recognize();
+			recognizer.UnloadAllGrammars();
+			textBox1.Text = result.Text;
+
+			//FileStream fs = new FileStream(@"C:\Users\hardy\source\repos\VoiceSaver_\VoiceSaver_\bin\Debug\Test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+			//byte[] arr = System.Text.Encoding.Default.GetBytes(text);
+			//fs.Write(arr, 0, arr.Length);
+			//MessageBox.Show("In File");
+		}
+	}
+}
